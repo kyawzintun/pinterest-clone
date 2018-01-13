8cbcc433fd630ee9ff26a6fac8d9c78b96582e97
Kyaw Zin Tun <kyawzintun@kyawzintun.local>
2018-01-13 12:02:52 +0630

    update link

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Image, Menu, Header, Label, Popup} from 'semantic-ui-react';
 import store from 'store';
+import { Link } from 'react-router-dom'
 
 import isLoggedIn from '../helper/isLoggedIn';
 import SearchInput from '../search/search';
@@ -19,7 +20,7 @@ class NavBar extends Component {
     const user = store.get('user');
     return (
       <Menu fixed='top' stackable className="custom-menu">
-        <Menu.Item as='a' href="/explore">
+        <Menu.Item as={Link} to='/explore'>
           <Image size='mini' src={logo} style={{ marginRight: '1.5em' }} />
         </Menu.Item>
         <Menu.Item className="search-menu-item">
@@ -28,7 +29,7 @@ class NavBar extends Component {
         {!isLoggedIn() &&
           <Menu.Menu position='right' className="right-menu">
             <Menu.Item className="menu-item">
-              <Label as='a' href="/">
+              <Label as={Link} to='/'>
                 <Header as='h3' className="item_1">Login</Header>
               </Label>
             </Menu.Item>
@@ -37,12 +38,12 @@ class NavBar extends Component {
         {isLoggedIn() &&
           <Menu.Menu position='right' className="right-menu">
             <Menu.Item className="menu-item">
-              <Label as='a' href="/explore">
+              <Label as={Link} to='/explore'>
                 <Header as='h3' className="item_1">Home</Header>
               </Label>
             </Menu.Item>
             <Menu.Item className="menu-item">
-              <Label as='a' href={`/${user.id}/boards`}>
+              <Label as={Link} to={`/${user.id}/boards`}>
                 <Image avatar spaced='right' src={user.picture.data.url} />
                 {user.name.split(' ')[0]}
               </Label>
